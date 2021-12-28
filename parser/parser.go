f8c2dbaf487afea2e8372a943a6469270e859b21
smalldevshima <92947358+smalldevshima@users.noreply.github.com>
2021-12-28 17:32:37 +0100

    [parser] 2.4 - Parser.parseStatement and Parser.parseLetStatement added

--- parser/parser.go
+++ parser/parser.go
@@ -8,6 +8,7 @@ import (
 
 /// Types
 
+// The Parser consumes the output of a given lexer.Lexer and produces an ast.Program as its output.
 type Parser struct {
 	lx *lexer.Lexer
 
@@ -25,11 +26,71 @@ func New(l *lexer.Lexer) *Parser {
 	return p
 }
 
+// nextToken advances the tokens read from the internal Lexer.
 func (p *Parser) nextToken() {
 	p.currentToken = p.peekToken
 	p.peekToken = p.lx.NextToken()
 }
 
+// ParseProgram consumes the internal Lexer's token list and produces a Program from them.
 func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.currentToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+// parseStatement checks the current token type and calls the corresponding parse method.
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currentToken.Type {
+	case token.LET:
+		// * check if s is nil, else the wrapped interface type will mask the nil value
+		if s := p.parseLetStatement(); s != nil {
+			return s
+		}
+	}
 	return nil
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.currentToken}
+
+	if !p.expectPeek(token.IDENTIFIER) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// todo: currently expressions are skipped until a semicolon is found
+	for p.currentToken.Type != token.SEMICOLON {
+		if p.currentToken.Type == token.EOF {
+			return nil
+		}
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// expectPeek compares the next token against the provided.
+// If they are the same, it advances the tokens and returns true.
+// Otherwise it leaves the tokens as is and returns false.
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekToken.Type == t {
+		p.nextToken()
+		return true
+	}
+	return false
 }
