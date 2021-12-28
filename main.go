04c743d5e57cd73000eb361c6c98aabd6559db0f
smalldevshima <92947358+smalldevshima@users.noreply.github.com>
2021-12-28 15:22:01 +0100

    [main] 1.5 - main now starts repl if in user session

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/smalldevshima/go-monkeyi/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language REPL!\n", user.Username)
+	fmt.Printf("Feel free to type in some code!\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
