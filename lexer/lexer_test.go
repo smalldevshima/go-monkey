20ff45bceee96b1ab6ac99659f584738b8723f9c
smalldevshima <92947358+smalldevshima@users.noreply.github.com>
2021-12-28 12:16:13 +0100

    [lexer] 1.2 - modified test case to include bigger subset of monkey language

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,31 +6,71 @@ import (
 	"github.com/smalldevshima/go-monkeyi/token"
 )
 
+/// Constants
+
+const (
+	MONKEY_INPUT = `
+let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+	x + y;
+};
+
+let result = add(five, ten);
+`
+)
+
 /// Package functions
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
-
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENTIFIER, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INTEGER, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "ten"},
+		{token.ASSIGN, "="},
+		{token.INTEGER, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENTIFIER, "x"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.IDENTIFIER, "x"},
+		{token.PLUS, "+"},
+		{token.IDENTIFIER, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENTIFIER, "add"},
+		{token.LPAREN, "("},
+		{token.IDENTIFIER, "five"},
 		{token.COMMA, ","},
+		{token.IDENTIFIER, "ten"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
-	lex := New(input)
+	lex := New(MONKEY_INPUT)
 
 	for index, test := range tests {
-		var tok token.Token
-		tok = lex.NextToken()
+		tok := lex.NextToken()
 
 		if tok.Type != test.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", index, test.expectedType, tok.Type)
