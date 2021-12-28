33ccd1dababfee7bfbcb77ff04a89a88c66fd6d1
smalldevshima <92947358+smalldevshima@users.noreply.github.com>
2021-12-28 11:49:54 +0100

    [lexer] 1.2 - added map of defined keywords and LookupIdent function to get the TokenType of any given identifer

--- token/token.go
+++ token/token.go
@@ -28,6 +28,25 @@ const (
 	LET      = "LET"
 )
 
+var (
+	// keywords is a map of literals to their corresponding TokenType.
+	keywords = map[string]TokenType{
+		"fn":  FUNCTION,
+		"let": LET,
+	}
+)
+
+/// Functions
+
+// LookupIdent checks if the given identifier is a keyword and if so, returns its TokenType.
+// If the given identifier is not a keyword, it returns the TokenType for identifiers.
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
 /// Types
 
 type TokenType string
