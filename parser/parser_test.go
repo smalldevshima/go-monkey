e5b49b27dc6a825abb9dfd5da1f9e463f5c8ad0d
smalldevshima <92947358+smalldevshima@users.noreply.github.com>
2021-12-29 10:57:39 +0100

    [parser] 2.7 - prepared test for parsing integer literal expressions

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -102,6 +102,34 @@ func TestIdentifierExpression(t *testing.T) {
 	}
 }
 
+func TestIntergerLiteralExpression(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statement. got=%d", len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("stmt.Expression is not *ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+	if literal.Value != 5 {
+		t.Errorf("literal.Value is not %d. got=%d", literal.Value)
+	}
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("literal.TokenLiteral is not '5'. got=%q", literal.TokenLiteral())
+	}
+}
+
 /// helpers
 
 func checkLetStatement(t *testing.T, s ast.Statement, name string) {
