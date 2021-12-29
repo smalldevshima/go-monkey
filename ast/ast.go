69335d769cba3d937cd95d7bcb52e9403e21d45d
smalldevshima <92947358+smalldevshima@users.noreply.github.com>
2021-12-29 11:01:03 +0100

    [ast] 2.7 - added ast.IntegerLiteral expression node type

--- ast/ast.go
+++ ast/ast.go
@@ -117,3 +117,13 @@ type Identifier struct {
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
+
+type IntegerLiteral struct {
+	// the token.INTEGER token
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.TokenLiteral() }
