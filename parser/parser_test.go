07bd397a98de43c593508985406c8f8ad8ec0eb1
smalldevshima <92947358+smalldevshima@users.noreply.github.com>
2021-12-29 10:49:51 +0100

    [parser] 2.7 - prepared test for parsing identifier expression statement

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -73,6 +73,35 @@ func TestReturnStatements(t *testing.T) {
 	}
 }
 
+func TestIdentifierExpression(t *testing.T) {
+	input := `foobar;`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statement. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("stmt.Expression is not *ast.Identifier. got=%T", stmt.Expression)
+	}
+	if ident.Value != "foobar" {
+		t.Errorf("ident.Value is not 'foobar'. got=%q", ident.Value)
+	}
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLiteral is not 'foobar'. got=%q", ident.TokenLiteral())
+	}
+}
+
 /// helpers
 
 func checkLetStatement(t *testing.T, s ast.Statement, name string) {
